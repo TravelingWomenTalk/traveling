8e559c55994f934f7a0080542eac2bd7909e3d78
brianbianchi <bianchibrianj@gmail.com>
2019-11-16 14:06:42 -0500

    rename

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-travelingwomen!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-traveling!');
   });
 
   afterEach(async () => {
