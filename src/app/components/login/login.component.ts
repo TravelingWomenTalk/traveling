b08e5ffebbed348ba7dcda4e4879187bfd444398
brianbianchi <bianchibrianj@gmail.com>
2019-11-30 18:55:39 -0500

    disable buttons on invalid forms

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -26,8 +26,8 @@ export class LoginComponent implements OnInit {
 
   public buildForm() {
     this.loginForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl('')
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
     });
   }
 
@@ -35,6 +35,14 @@ export class LoginComponent implements OnInit {
     this.authService.emailLogin(this.loginForm.getRawValue().email, this.loginForm.getRawValue().password);
 
     this.router.navigate(['/reviews']);
+  }
+
+  public get emailControl(): AbstractControl {
+    return this.loginForm.get('email');
+  }
+
+  public get passwordControl(): AbstractControl {
+    return this.loginForm.get('password');
   }
 
 }
