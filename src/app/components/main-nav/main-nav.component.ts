515e397174d71cf91b2479f71e6bf27f156a71aa
brianbianchi <bianchibrianj@gmail.com>
2019-12-06 22:59:23 -0500

    fix profile nav link

--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.css']
 })
-export class MainNavComponent {
+export class MainNavComponent implements OnInit {
   public isNavbarCollapsed: boolean = true;
+  public user: any;
 
   constructor(
     public authService: AuthService,
     public router: Router) { }
 
-  public navigateProfile(): void {
+  public ngOnInit(): void {
+    this.getUser();
+  }
+
+  public getUser(): void {
     this.authService.user$.subscribe((user) => {
-      this.router.navigate(['profile', user.uid]);
+      this.user = user;
     });
   }
 
